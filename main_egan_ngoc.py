324d48240023d67d3735d31348a47919edfa8eca
Tommy Ngx <tommyfederation@gmail.com>
2021-02-21 17:57:11 +1100

    ok

--- main_egan_ngoc.py
+++ main_egan_ngoc.py
@@ -20,11 +20,24 @@ from impyute.imputation.cs import mice
 import pandas as pd
 from autoimpute.imputations import MiceImputer, SingleImputer, MultipleImputer
 from autoimpute.analysis import MiLinearRegression
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 import warnings
 warnings.filterwarnings('ignore',category=FutureWarning)
 warnings.filterwarnings('ignore', category=DeprecationWarning)
+
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+from tensorflow.python.util import deprecation
+deprecation._PRINT_DEPRECATION_WARNINGS = False
+try:
+    from tensorflow.python.util import module_wrapper as deprecation
+except ImportError:
+    from tensorflow.python.util import deprecation_wrapper as deprecation
+deprecation._PER_MODULE_WARNING_LIMIT = 0
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 
 
 def main (args):
