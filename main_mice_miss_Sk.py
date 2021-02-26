528a3145a2f9c450bd8abf680bae8c29e284cbe8
Tommy Ngx <tommyfederation@gmail.com>
2021-02-26 20:14:46 +1100

    Update main_mice_miss_Sk.py

--- main_mice_miss_Sk.py
+++ main_mice_miss_Sk.py
@@ -86,7 +86,10 @@ def auc_mlp(impute,data):
 
     X_train, test_x, y_train, test_lab = train_test_split(X,targets,test_size = 0.3,random_state = 42)
     #clf = DecisionTreeClassifier( random_state = 42) # max_depth =3,
-    clf = MLPClassifier(hidden_layer_sizes= X_train.shape[1]//2,  early_stopping=True) #max_iter=500,
+    #clf = MLPClassifier(hidden_layer_sizes= X_train.shape[1]//2,  early_stopping=True) #max_iter=500,
+    clf = MLPClassifier(hidden_layer_sizes=x_train.shape[1]//2, max_iter=500, early_stopping=True, 
+      #learning_rate_init=0.01,
+      learning_rate='constant')
     clf.fit(X_train, y_train)
     test_pred_decision_tree = clf.predict(test_x)
     return  metrics.accuracy_score(test_lab, test_pred_decision_tree)
