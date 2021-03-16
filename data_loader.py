20305942d19e095f41a59d63cb46118fb656cab9
Tommy Ngx <tommyfederation@gmail.com>
2021-03-16 21:49:13 +1100

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -74,7 +74,7 @@ def data_loader2 (data_name, miss_rate, random):
       
   return data_x, miss_data_x, data_m
 
-  def data_loader3 (data_name, miss_rate, random):
+def data_loader3 (data_name, miss_rate, random):
   # Load data
   if data_name in ['letter', 'spam', 'breast', 'credit', 'news','blood','vowel','ecoli','ionosphere','parkinsons','parkinsons2','seedst','vehicle','vertebral','wine','banknote','balance','yeast']:
     file_name = 'data/'+data_name+'_full.csv'
